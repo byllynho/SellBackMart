080de8bb3e2ea6c074b4a3014b72fecf02ea338e
Andrew Edmiston <andrewedmiston3@gmail.com>
2019-04-06 00:43:05 +0000

    Added styling and user authentication to items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-
+    layout 'standart'
+    before_action :authenticate_user!
     def view_item
         begin
             @item = Item.find(params[:id])
