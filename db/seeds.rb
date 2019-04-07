56d274915b5cace31ede96a098cd8adf27cf4180
Andrew Edmiston <andrewedmiston3@gmail.com>
2019-04-07 10:44:28 +0000

    Got view_item page to satisfactory layout (for now)

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,9 @@ user_1 = User.create!(name: "Admin Istrator", email: "admin@memphis.edu", major:
 item_1 = user_1.items.build(description: "This is my item. Please buy it.", price: 3.25, title: "My Awesome Item", category_id: 1)
 item_1.update(photo_file_name: 'bopit.jpg')
 user_1.save!
+
+user_2 = User.create!(name: "Andrew", email: "andrew@memphis.edu", major: "Computer Science", 
+    department: "Computer Science", confirmed_at: "Thu, 04 Apr 2019 04:55:06 UTC +00:00 ",
+    confirmation_sent_at: "Thu, 04 Apr 2019 04:54:31 UTC +00:00",
+    password: "123456" )
+user_2.save!
