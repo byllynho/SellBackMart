594bc9d3e9cb3b1dbd223a6b2736fc0f3eb1d553
Andrew Edmiston <andrewedmiston3@gmail.com>
2019-05-01 04:40:09 +0000

    Fixed picture bug in catalog and message seller bug on profile vieww

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @message = Message.new
     @deals = []
     @user.items.each do |record| 
       if record.settlement
